790f3db4b3a9ea6aa2292ce9890f5454ea4d51d1
Roy Segall <roysegall@Roys-MacBook-Pro-2.local>
2014-02-18 21:58:02 +0200

    Issue #2199409 by RoySegall: Create hook_message_ui_control_access.

--- message_ui.api.php
+++ message_ui.api.php
@@ -16,3 +16,20 @@
 function hook_message_ui_view_alter(&$build, $message) {
   // Check the output of the message as you wish.
 }
+
+/**
+ * Implements hook_message_message_ui_access_control().
+ *
+ * @param Message|String $message
+ *  The message. When creating the type will be passed.
+ * @param $op
+ *  The operation: create, update, read or delete.
+ * @param $account
+ *  The user account object.
+ *
+ * @return bool
+ *  True or false.
+ */
+function hook_message_message_ui_access_control($message, $op, $account) {
+  return MESSAGE_UI_DENY;
+}
