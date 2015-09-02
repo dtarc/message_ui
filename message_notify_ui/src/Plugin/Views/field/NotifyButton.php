b67dc18ec9483cac67e6ab0a4d8dfcb143e0d1b2
Paul McCrodden <info@paulmccrodden.com>
2015-09-02 21:53:53 +0100

    Started restructuring message_notify_ui: info file to yaml and views handler to plugin

---
+++ message_notify_ui/src/Plugin/Views/field/NotifyButton.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\message_ui\Plugin\views\field\NotifyButton.
+ */
+
+namespace Drupal\message_ui\Plugin\views\field;
+
+use Drupal\Core\Url;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Drupal\message\Entity\Message;
+use Drupal\message_ui\MessageAccessControlHandler;
+
+/**
+ * Field handler to present a Notify button for a message instance.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("notify_button")
+ */
+class NotifyButton extends FieldPluginBase {
+
+  /**
+   * Stores the result of node_view_multiple for all rows to reuse it later.
+   *
+   * @var array
+   */
+  protected $build;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $message = Message::load($values->_entity->id());
+
+    if (user_access('send message notify')) {
+      return l(t('Notify'), 'message/' . $message->id() . '/notify');
+    }
+    return NULL;
+  }
+}
