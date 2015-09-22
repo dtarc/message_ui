bb69a0ab2281b27f08710b29985622e688194090
Paul McCrodden <info@paulmccrodden.com>
2015-09-22 18:04:06 +0200

    Added hook_entity_type_build to module file to allow adding form handlers. Added new MessageDeleteForm class.

---
+++ src/Form/MessageDeleteForm.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\message_ui\Form\MessageDeleteForm.
+ */
+
+namespace Drupal\message_ui\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting a message.
+ */
+class MessageDeleteForm extends ContentEntityDeleteForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getDeletionMessage() {
+    /** @var \Drupal\message\MessageInterface $entity */
+    $entity = $this->getEntity();
+
+    $message_type_storage = $this->entityManager->getStorage('message_type');
+    $message_type = $message_type_storage->load($entity->bundle())->label();
+
+    if (!$entity->isDefaultTranslation()) {
+      return $this->t('@language translation of the @type %label has been deleted.', [
+        '@language' => $entity->language()->getName(),
+        '@type' => $message_type,
+        '%label' => $entity->label(),
+      ]);
+    }
+
+    return $this->t('The @type %title has been deleted.', array(
+      '@type' => $message_type,
+      '%title' => $this->getEntity()->label(),
+    ));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function logDeletionMessage() {
+    /** @var \Drupal\node\NodeInterface $entity */
+    $entity = $this->getEntity();
+    $this->logger('content')->notice('@type: deleted %title.', ['@type' => $entity->getType(), '%title' => $entity->label()]);
+  }
+
+}
